c972171b2d2123f5ed52fd7fbb42bca2b43dd4b1
upend1310 <shawkumarupendra@outlook.com>
2023-02-11 19:22:37 -0500

    Leetcode 326. Power of Three

---
+++ powerOfThree.js
@@ -0,0 +1,33 @@
+/*
+326. Power of Three
+Given an integer n, return true if it is a power of three. Otherwise, return false.
+An integer n is a power of three, if there exists an integer x such that n == 3x.
+
+Example 1:
+Input: n = 27
+Output: true
+Explanation: 27 = 33
+
+Example 2:
+Input: n = 0
+Output: false
+Explanation: There is no x where 3x = 0.
+
+Example 3:
+Input: n = -1
+Output: false
+Explanation: There is no x where 3x = (-1).
+*/
+
+var isPowerOfThree = function(n) {
+    let temp = 1
+    while(temp <= n) {
+        if(temp === n) {
+            return true
+        }
+        temp = temp * 3
+    }
+    return false
+};
+
+console.log(isPowerOfThree(-1))
