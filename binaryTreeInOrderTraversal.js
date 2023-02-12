394b856a023767a7e00369d5139d33e6d98031b0
upend1310 <shawkumarupendra@outlook.com>
2023-02-11 20:12:36 -0500

    Leetcode 94. Binary Tree Inorder Traversal

---
+++ binaryTreeInOrderTraversal.js
@@ -0,0 +1,43 @@
+/*
+94. Binary Tree Inorder Traversal
+Given the root of a binary tree, return the inorder traversal of its nodes' values.
+
+Example 1:
+Input: root = [1,null,2,3]
+Output: [1,3,2]
+
+Example 2:
+Input: root = []
+Output: []
+
+Example 3:
+Input: root = [1]
+Output: [1]
+*/
+
+
+function TreeNode(val, left, right) {
+    this.val = (val === undefined ? 0 : val)
+    this.left = (left === undefined ? null : left)
+    this.right = (right === undefined ? null : right)
+}
+
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var inorderTraversal = function (root) {
+    let result = []
+    traverse(root)
+
+    function traverse(root) {
+        if (root != null) {
+            traverse(root.left)
+            result.push(root.val)
+            traverse(root.right)
+        }
+    }
+
+    return result
+};
+
