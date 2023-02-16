af07fdc4f94793ab3efd054b27e97755a884289b
upend1310 <shawkumarupendra@outlook.com>
2023-02-15 23:52:57 -0500

    Leetcode 22. Generate Parentheses

--- generateParentheses.js
+++ generateParentheses.js
@@ -15,19 +15,18 @@ Output: ["()"]
  * @param {number} n
  * @return {string[]}
  */
- var generateParenthesis = function(n) {
+var generateParenthesis = function (n) {
     let res = []
 
-    let iterate = (str, open, close) =>{
-        if(open > n || close >n || close >open) return
-        if(str.length == n *2 && open ==close){
+    let iterate = (str, open, close) => {
+        if (open > n || close > n || close > open) return
+        if (str.length == n * 2 && open == close) {
             res.push(str)
-            return
         }
-        iterate(str +'(',open+1,close)
-        iterate(str + ')', open, close+1)
+        iterate(str + '(', open + 1, close)
+        iterate(str + ')', open, close + 1)
     }
 
-    iterate('',0,0)
+    iterate('', 0, 0)
     return res
 };
