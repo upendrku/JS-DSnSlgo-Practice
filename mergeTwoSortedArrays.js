c6254b3fd2c5a7fc88c2384cedaa6d1faf04323b
upend1310 <shawkumarupendra@outlook.com>
2023-03-01 13:44:22 -0500

    Leetcode 88. Merge Sorted Array

--- mergeTwoSortedArrays.js
+++ mergeTwoSortedArrays.js
@@ -27,22 +27,53 @@ The result of the merge is [1].
 Note that because m = 0, there are no elements in nums1. The 0 is only there to ensure the merge result can fit in nums1.
 */
 
+// function merge(nums1, m, nums2, n) {
+//     let insertPositionOfNums1 = m + n - 1 // 5
+//     m-- // 2
+//     n-- // 2
+//     while (n >= 0) {
+//         if (nums1[m] > nums2[n]) {
+//             nums1[insertPositionOfNums1] = nums1[m]
+//             m--
+//         } else {
+//             nums1[insertPositionOfNums1] = nums2[n]
+//             n--
+//         }
+//         insertPositionOfNums1--
+//         // console.log(nums1)
+//     }
+//     return nums1
+// }
+
+/**
+ * @param {number[]} nums1
+ * @param {number} m
+ * @param {number[]} nums2
+ * @param {number} n
+ * @return {void} Do not return anything, modify nums1 in-place instead.
+ */
 function merge(nums1, m, nums2, n) {
-    let insertPositionOfNums1 = m + n - 1 // 5
-    m-- // 2
-    n-- // 2
-    while (n >= 0) {
-        if (nums1[m] > nums2[n]) {
-            nums1[insertPositionOfNums1] = nums1[m]
-            m--
+    let first = m - 1
+    let second = n - 1
+    let i = m + n - 1
+
+    while (second >= 0) {
+        let fVal = nums1[first]
+        let sVal = nums2[second]
+
+        console.log(fVal)
+        console.log(sVal)
+        console.log('.......')
+        if (fVal > sVal) {
+            nums1[i] = fVal
+            i--
+            first--
         } else {
-            nums1[insertPositionOfNums1] = nums2[n]
-            n--
+            nums1[i] = sVal
+            i--
+            second--
         }
-        insertPositionOfNums1--
-        // console.log(nums1)
     }
-    return nums1
 }
 
 // console.log(merge([2, 5, 6], 3, [1, 2, 3, 0, 0, 0], 3))
