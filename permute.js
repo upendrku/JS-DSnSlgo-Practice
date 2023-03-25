0ea64e8e36b69ffb499e17425f65c25beaadbdd1
upend1310 <shawkumarupendra@outlook.com>
2023-03-24 21:58:40 -0400

    Leetcode 46. Permutations

---
+++ permute.js
@@ -0,0 +1,51 @@
+/*
+46. Permutations
+Given an array nums of distinct integers, return all the possible permutations. You can return the answer in any order.
+
+Example 1:
+Input: nums = [1,2,3]
+Output: [[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
+
+Example 2:
+Input: nums = [0,1]
+Output: [[0,1],[1,0]]
+
+Example 3:
+Input: nums = [1]
+Output: [[1]]
+*/
+
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var permute = function (nums) {
+    if (nums.length === 0) return [[]]
+    if (nums.length === 1) return [nums]
+    if (nums.length === 2) return [nums, [nums[1], nums[0]]]
+
+    return nums.map(num => {
+        const rest = nums.filter(i => i !== num)
+        const per = permute(rest)
+        return per.map(i => [num, ...i])
+    }).flat(1)
+};
+
+console.log(permute([1, 2, 3]))
+
+/*
+Array.prototype.flat()
+The flat() method creates a new array with all sub-array elements concatenated into it 
+recursively up to the specified depth.
+
+const arr1 = [0, 1, 2, [3, 4]];
+
+console.log(arr1.flat());
+// Expected output: Array [0, 1, 2, 3, 4]
+
+const arr2 = [0, 1, 2, [[[3, 4]]]];
+
+console.log(arr2.flat(2));
+// Expected output: Array [0, 1, 2, Array [3, 4]]
+
+*/
