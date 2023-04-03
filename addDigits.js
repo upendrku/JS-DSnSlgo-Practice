a3c67b6a5f47a255a8829cf33a6f9838550f6a87
upend1310 <shawkumarupendra@outlook.com>
2023-04-03 00:36:08 -0400

    Leetcode 258. Add Digits

---
+++ addDigits.js
@@ -0,0 +1,52 @@
+/* 
+258. Add Digits
+
+Given an integer num, repeatedly add all its digits until the result has only one digit, and return it.
+
+Example 1:
+Input: num = 38
+Output: 2
+Explanation: The process is
+38 --> 3 + 8 --> 11
+11 --> 1 + 1 --> 2 
+Since 2 has only one digit, return it.
+
+Example 2:
+Input: num = 0
+Output: 0
+*/
+
+/**
+ * @param {number} num
+ * @return {number}
+ */
+var addDigits = function (num) {
+    let sum = sumAllDigits(num)
+
+    while (sum > 9) {
+        sum = sumAllDigits(sum)
+    }
+
+    return sum
+};
+
+const sumAllDigits = (num) => {
+    let sum = 0
+    while (num) {
+        sum += num % 10
+        num = Math.floor(num / 10)
+    }
+    return sum
+}
+
+// Better solution
+
+var addDigits = function (num) {
+    if (num == 0) {
+        return 0
+    }
+    if (num < 10) {
+        return num
+    }
+    return num % 9 === 0 ? 9 : num % 9
+}
